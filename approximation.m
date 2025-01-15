7e3a5f6c91b78c2db332acec82397a9eab9f2298
Zuzia <192205728+marchmakrysia@users.noreply.github.com>
2025-01-15 12:43:32 +0100

    Update approximation.m

--- approximation.m
+++ approximation.m
@@ -17,4 +17,9 @@ w2=A2(1)+A2(2)*xx+A2(3)*xx.^2
 X4=[ones(length(x),1) x' x'.^2 x'.^3 x'.^4]
 A4=inv(X4'*X4)*X4'*Y
 w4=A4(1)+A4(2)*xx+A4(3)*xx.^2+A4(4)*xx.^3+A4(5)*xx.^4
-plot(x,y,'*',xx,w,xx,w2,xx,w4)
+
+X6=[ones(length(x),1) x' x'.^2 x'.^3 x'.^4 x.^5 x.^6]
+A6=inv(X6'*X6)*X6'*Y
+w6=A6(1)+A6(2)*xx+A6(3)*xx.^2+A6(4)*xx.^3+A6(5)*xx.^4+A6(6)*xx.^5+A6(7)*xx.^6
+
+plot(x,y,'*',xx,w,xx,w2,xx,w4,xx,w6)
